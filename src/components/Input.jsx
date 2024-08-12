f803f50f42cb25487d2b0062ecfe838dd5f6fd68
praju.ladkat <praju.ladkat@gmail.com>
2024-08-12 16:44:33 +0000

    modiefed popup modal

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ const Input = ({ name, label, type, className, placeholder }) => {
     return (
         <div className="flex w-full mb-6 flex-col gap-4 items-start justify-start">
             <label className="font-bold text-white" htmlFor={name}>{label}</label>
-            <input type={type} name={name} placeholder={placeholder} className={'p-3 outline-none bg-black/80 text-gray-100 border border-transparent focus:border-2 duration-100 focus:shadow-lg focus:border-orange-300 rounded-2xl w-[100%]'} />
+            <input type={type} name={name} placeholder={placeholder} className={'p-3 outline-none bg-black/80 text-gray-100 border border-transparent focus:border-2 duration-100 focus:shadow-lg focus:border-orange-300/70 rounded-2xl w-[100%]'} />
         </div>
     )
 }
